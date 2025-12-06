c1bc58917d22f188bb184f888299e6179419e398
Oleg Shilov <shilov-1@yandex.ru>
2025-12-06 17:59:24 +0300

    refactoring;

--- src/routes/asImage/asImage.ts
+++ src/routes/asImage/asImage.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 
-import type { Orientation } from 'types/@types.js';
+import { get as _get } from 'lodash-es';
+
+import type { Orientation, Preset } from 'types/@types.js';
 
 import { get as mediaGet } from 'api/media/get.js';
 import { get as presetGet } from 'api/preset/get.js';
@@ -25,26 +27,26 @@ asImageRouter.get(/\/ai-slide?.+/, async (req, res) => {
   const query = req.query;
 
   const {
-    tz = '3',
+    tz = '3', // timezone
     o, // o = orientation
     r, // r = resolution
-    p = 'default',
+    p = 'default', // preset
   } = query;
 
   const preset = p as string;
 
-  const presetInfo = await presetGet(preset);
+  const presetInfo = await presetGet(preset) as unknown as Preset;
 
   let resolution = r as string;
 
   if (!r) {
-    resolution = presetInfo.options.resolution || '1920*1080';
+    resolution = _get(presetInfo, 'options.resolution') || '1920*1080';
   }
 
   let orientation = o as Orientation;
 
   if (!o) {
-    orientation = presetInfo.options.orientation || 'h';
+    orientation = _get(presetInfo, 'options.orientation') || 'h';
   }
 
   const timezone = tz as string;
